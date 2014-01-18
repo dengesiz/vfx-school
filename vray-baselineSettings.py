b27ce1a104def1971d36b3d55ba803371f648b0a
dengesiz <engin_ar@yahoo.com>
2014-01-18 16:40:56 -0500

    taking the dependencies into account as well
    previously the code wasn't working properly as dictionary was randomly
    accessing values that had a dependency to be activated first.

--- vray-baselineSettings.py
+++ vray-baselineSettings.py
@@ -1,28 +1,34 @@
 import maya.cmds as cmds
 myRenderer = "vraySettings"
 
-settings = {"sRGBOn":1,
-"vfbOn":1,
-"globopt_light_doDefaultLights": 0,
-"globopt_mtl_limitDepth": 1,
-"globopt_mtl_maxDepth": 5,
-"samplerType": 1,
-"aaFilterType": 6,
-"aaFilterSize": 2,
-"dmcLockThreshold": 1,
-"cmap_type": 1,
-"cmap_gamma": 2.2,
-"cmap_subpixelMapping": 1,
-"cmap_adaptationOnly": 1,
-"cmap_clampOutput": 1,
-"cmap_clampLevel": 6,
-"giOn": 1,
-"primaryEngine": 2,
-"dmc_depth": 2,
-"ddisplac_maxSubdivs": 4,
-"sys_rayc_dynMemLimit": 26000,
-"sys_regsgen_xc":16,
+settings = {"sRGBOn":(1,),
+"vfbOn":(1,),
+"globopt_light_doDefaultLights": (0,),
+"globopt_mtl_limitDepth": (1,),
+"globopt_mtl_maxDepth": (5, ("globopt_mtl_limitDepth",)),
+"samplerType": (1,),
+"aaFilterType": (6,),
+"aaFilterSize": (2,),
+"dmcLockThreshold": (1, ("samplerType",)),
+"cmap_type": (1,),
+"cmap_gamma": (2.2,),
+"cmap_subpixelMapping": (1,),
+"cmap_adaptationOnly": (1,),
+"cmap_clampOutput": (1,),
+"cmap_clampLevel": (6,),
+"giOn": (1,),
+"primaryEngine": (2, ("giOn",)),
+"dmc_depth": (2, ("giOn",)),
+"ddisplac_maxSubdivs": (4,),
+"sys_rayc_dynMemLimit": (26000,),
+"sys_regsgen_xc":(16,),
 }
 
 for item in settings.iteritems():
-    cmds.setAttr("%s.%s" %(myRenderer, item[0]), item[1])
+    if len(item[1]) == 1:
+        cmds.setAttr("%s.%s" %(myRenderer, item[0]), item[1][0])
+    else:
+        dependency = item[1][1][0]
+        dependencyValue = settings[dependency][0]
+        cmds.setAttr("%s.%s" %(myRenderer, dependency), dependencyValue)
+        cmds.setAttr("%s.%s" %(myRenderer, item[0]), item[1][0])
